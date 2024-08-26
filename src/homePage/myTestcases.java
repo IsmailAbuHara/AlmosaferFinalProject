0ba90483c4e13f8ee823e962e6549bcbadc5bc45
IsmailAbuHara <esmeelabuherra15@gmail.com>
2024-08-26 19:33:30 +0300

    update

--- src/homePage/myTestcases.java
+++ src/homePage/myTestcases.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -30,7 +31,7 @@ public class myTestcases {
 
 		driver.manage().window().maximize();
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
 		driver.get(Almosafer);
 
@@ -132,6 +133,119 @@ public class myTestcases {
 		int RandomIndex = rand.nextInt(URLS.length);
 		
 		driver.get(URLS[RandomIndex]);
+	}
+	
+	@Test(priority = 8)
+
+	public void FillHotelTab() {
+		
+		String [] EnglishCities = {"Dubai" , "Jeddah" , "Riyadh"};
+		int randomEnglishCity = rand.nextInt(EnglishCities.length);
+		
+		String [] ArabicCities = {"دبي" , "جدة"};
+		int randomArabicCity = rand.nextInt(ArabicCities.length);
+
+
+		WebElement HotelTab = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+
+		HotelTab.click();
+		WebElement SearchHotelInputField = driver.findElement(By.xpath("//input[@data-testid='AutoCompleteInput']"));
+
+		String WebsiteURL = driver.getCurrentUrl();
+
+		if (WebsiteURL.contains("ar")) {
+
+			SearchHotelInputField.sendKeys(ArabicCities[randomArabicCity]);
+		} else {
+			SearchHotelInputField.sendKeys(EnglishCities[randomEnglishCity]);
+
+		}
+
+		WebElement ListOfLocations = driver.findElement(By.cssSelector(".sc-phbroq-4.gGwzVo.AutoComplete__List"));
+
+		WebElement firstResult = ListOfLocations.findElements(By.tagName("li")).get(1);
+		firstResult.click();
+
+	}
+	
+	@Test(priority = 9)
+
+	public void RandomlySelectTheNumberOfVistor() {
+
+		WebElement SelectorofTheVistor = driver
+				.findElement(By.xpath("//select[@data-testid='HotelSearchBox__ReservationSelect_Select']"));
+
+		Select select = new Select(SelectorofTheVistor);
+
+		// By index
+
+		int randomIndex = rand.nextInt(2);
+		select.selectByIndex(randomIndex);
+
+		// By value
+//		select.selectByValue("B"); 
+
+		// by visibleText
+//		if(driver.getCurrentUrl().contains("ar")) {
+//			select.selectByVisibleText("1 غرفة، 1 بالغ، 0 أطفال"); 
+//
+//		}else {
+//			select.selectByValue("1 Room, 1 Adult, 0 Children"); 
+//		}
+//		
+
+		WebElement SearchHotelButton = driver
+				.findElement(By.xpath("//button[@data-testid='HotelSearchBox__SearchButton']"));
+		SearchHotelButton.click();
+	}
+	
+	@Test(priority = 10)
+
+	public void CheckThePageFullyLoaded() throws InterruptedException {
+
+		boolean expectedResult = true;
+		Thread.sleep(10000);
+		String results = driver.findElement(By.xpath("//span[@data-testid='HotelSearchResult__resultsFoundCount']"))
+				.getText();
+
+		boolean finished = results.contains("وجدنا") || results.contains("found");
+
+		Assert.assertEquals(finished, expectedResult);
+
+	}
+	
+	@Test(priority = 11, description = "We explain if anything is related to the test")
+
+	public void SortItemsLowestToHighestPrice() {
+
+		boolean expectedResults = true;
+		WebElement LowestPriceSortButton = driver
+				.findElement(By.xpath("//button[@data-testid='HotelSearchResult__sort__LOWEST_PRICE']"));
+
+		LowestPriceSortButton.click();
+
+		WebElement PricesContainer = driver.findElement(By.cssSelector(".sc-htpNat.KtFsv.col-9"));
+
+		List<WebElement> AllPrices = PricesContainer.findElements(By.className("Price__Value"));
+
+//		List<WebElement> thePrices = driver.findElements(By.cssSelector(".Price__Wrapper.PriceDisplay__FinalRate.sc-dRCTWM.GFIG"));
+//		
+//		
+		String LowestPrice = AllPrices.get(0).getText();
+		String HighestPrice = AllPrices.get(AllPrices.size() - 1).getText();
+
+		System.out.println(LowestPrice);
+		System.out.println(HighestPrice);
+
+//		
+
+		int LowestPriceAsInt = Integer.parseInt(LowestPrice);
+		int HighestPriceAsInt = Integer.parseInt(HighestPrice);
+
+		boolean ActualResults = LowestPriceAsInt < HighestPriceAsInt;
+
+		Assert.assertEquals(ActualResults, expectedResults);
+
 	}
 
 }
